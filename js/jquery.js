ad4c37ec924df3d7cf7ec53b750ecebb3f1cf3a2
Daniel Gestino Notario <dageno11@gmail.com>
2018-02-20 00:45:41 +0100

    Add files via upload

--- js/jquery.js
+++ js/jquery.js
@@ -1,7 +1,34 @@
-$(document).ready(function(){
-    $('.barra').each(function(){
-        $(this).find('.barra-progress').animate({
-            width:$(this).attr('data-percent')
-        },6000);
-    });
+$(document).ready(function () {
+
+    firstScroll = false;
+
+    function isScrolledIntoView(elem)
+    {
+        var docViewTop = $(window).scrollTop();
+        var docViewBottom = docViewTop + $(window).height();
+    
+        var elemTop = $(elem).offset().top;
+        var elemBottom = elemTop + $(elem).height();
+    
+        return ((elemTop <= docViewBottom) && (elemTop >= docViewTop));
+    }
+
+    $(window).on('scroll', function() {
+        if (isScrolledIntoView('.skillbars') && !firstScroll) {
+            $('.barra').each(function () {
+                $(this).find('.barra-progress').animate({
+                    width: $(this).attr('data-percent')
+                }, 5000);
+            });
+            
+        }else if (isScrolledIntoView('.about') && !firstScroll) {
+            $('.foto').delay(1000).show("medium");
+            $('.textabout').delay(2000).fadeIn("slow");
+            $('.iconos').each(function () {
+                $(this).delay(3000).fadeIn("slow");
+            });
+            
+        }
+      });
+   
 });
